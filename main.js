ee8213e641bfd4eb70cb30c2735c811d1f0ee6f2
HiImZsofi <zsofia.gyori00@gmail.com>
2022-10-04 12:25:11 +0200

    calorie counter

--- main.js
+++ main.js
@@ -3,13 +3,16 @@ $(document).ready(function() {
     $( "#one" ).click(function() {
         $(".itemlist ").append('<li> Csirkehús <i>239</i> </li>');
         calories += 239;
+        $("#caloriecounter ").text('A jelenlegi kalória mennyisége: ' + JSON.stringify(calories));
       });
       $( "#two" ).click(function() {
         $(".itemlist ").append('<li> Narancslé <i>71</i> </li>');
         calories += 71;
+        $("#caloriecounter ").text('A jelenlegi kalória mennyisége: ' + JSON.stringify(calories));
       });
       $( "#three" ).click(function() {
         $(".itemlist ").append('<li> Marhahús <i>271</i> </li>');
         calories += 271;
+        $("#caloriecounter ").text('A jelenlegi kalória mennyisége: ' + JSON.stringify(calories));
       });
 })
